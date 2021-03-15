e4a06596a2957e47b3623e810b6aee9cebab4020
魏范光 <wei.fanguang@datatom.com>
2021-03-15 16:33:33 +0800

    fix: 修复g6图中label长度溢出时hover自动添加自定义节点以优化显示效果问题

--- src/pages/G6graph.js
+++ src/pages/G6graph.js
@@ -1,10 +1,21 @@
 import React, { PureComponent } from "react";
+import { Spin } from "antd";
 
 import G6 from "@antv/g6";
 
 import currenttb from "../assets/currenttb.svg";
 import nexttb from "../assets/nexttb.svg";
 import pretb from "../assets/pretb.svg";
+
+// 处理文本省略
+function getEllipsisLabel(label) {
+  return label.length > 12
+    ? `${label.slice(0, 5)}...${label.slice(
+        label.length - 5,
+        label.length - 1
+      )}`
+    : label;
+}
 
 // 注册一个边类型
 G6.registerEdge(
@@ -31,46 +42,82 @@ G6.registerEdge(
 // 注册一个节点类型
 G6.registerNode(
   "custom-node",
-  {
-    options: {
-      style: {},
-      stateStyles: {},
-    },
-    drawShape: (cfg, group) => {
-      const shape = group.addShape("image", {
-        attrs: {},
-      });
-      // console.log(cfg, group, shape, "registerNode-drawShape...");
+  // {
+  //   options: {
+  //     style: {},
+  //     stateStyles: {},
+  //   },
+  //   drawShape: (cfg, group) => {
+  //     const shape = group.addShape("image", {
+  //       attrs: {},
+  //     });
+  //     // console.log(cfg, group, shape, "registerNode-drawShape...");
 
-      return shape;
-    },
+  //     return shape;
+  //   },
 
-    // 动画
-    afterDraw: (cfg, group) => {
-      const shape = group.get("children")[0];
-      // console.log(shape, "afterdraw...");
+  //   // 动画
+  //   afterDraw: (cfg, group) => {
+  //     const shape = group.get("children")[0];
+  //     // console.log(shape, "afterdraw...");
 
-      shape.animate(
-        (ratio) => {
-          const diff = ratio <= 0.5 ? ratio * 10 : (1 - ratio) * 10;
-          return {
-            r: (cfg.size / 2) * diff,
-          };
-        },
-        {
-          repeat: true,
-          duration: 1000,
-          easing: "easeCubic",
-        }
-      );
-    },
+  //     shape.animate(
+  //       (ratio) => {
+  //         const diff = ratio <= 0.5 ? ratio * 10 : (1 - ratio) * 10;
+  //         return {
+  //           r: (cfg.size / 2) * diff,
+  //         };
+  //       },
+  //       {
+  //         repeat: true,
+  //         duration: 1000,
+  //         easing: "easeCubic",
+  //       }
+  //     );
+  //   },
 
-    setState: (name, value, node) => {
-      // console.log(name, value, node, "node-setState...");
-    },
+  //   setState: (name, value, node) => {
+  //     // console.log(name, value, node, "node-setState...");
+  //   },
+  // },
+  // "image"
+  (cfg) => {
+    return `
+      <rect
+        style={{
+          width: ${cfg?.index === "p0" ? "56" : "40"},
+          height: ${cfg?.index === "p0" ? "71" : "55"},
+        }}
+        keyshape="true"
+        name="nodeItem"
+      >
+        <img style={{ img: ${getNodeIcon(cfg?.index)}, cursor: 'pointer' }} />
+        <text
+          style={{
+            textAlign: 'center',
+            marginTop: ${cfg?.index === "p0" ? "68" : "52"},
+            fill: '#333',
+            cursor: 'pointer'
+          }}
+          name="title"
+        >
+          ${getEllipsisLabel(cfg.label)}
+        </text>
+      </rect>
+    `;
   },
-  "image"
+  "rect"
 );
+
+function getNodeIcon(idx) {
+  return idx === "p0"
+    ? currenttb
+    : idx?.includes("p") && idx !== "p0"
+    ? pretb
+    : idx?.includes("n")
+    ? nexttb
+    : null;
+}
 
 export default class G6Graph extends PureComponent {
   constructor(props) {
@@ -128,14 +175,14 @@ export default class G6Graph extends PureComponent {
         x: (graphWidth * Number(wPercent)) / 100 - Number(wPixel),
         y: (graphHeight * Number(hPercent)) / 100 - Number(hPixel),
 
-        type: "image",
-        img: this.getNodeItemIcon(item.id),
-        size: item?.index === "p0" ? 56 : 40,
-        // type: "custom-node",
-        label: item.label,
-        labelCfg: {
-          position: "bottom",
-        },
+        // type: "image",
+        // img: this.getNodeItemIcon(item.id),
+        // size: item?.index === "p0" ? 56 : 40,
+        type: "custom-node",
+        // label: item.label,
+        // labelCfg: {
+        // position: "bottom",
+        // },
       });
     });
 
@@ -190,7 +237,7 @@ export default class G6Graph extends PureComponent {
         style: {
           stroke: "#999",
           lineWidth: 1,
-          lineAppendWidth: 6,
+          lineAppendWidth: 4,
           endArrow: {
             path: G6.Arrow.vee(8, 8, 0),
             fill: "#999",
@@ -234,6 +281,12 @@ export default class G6Graph extends PureComponent {
 
     $graph.on("node:mouseenter", (ev) => {
       const node = ev.item;
+
+      // 过滤hover自定义的节点
+      if (node.destroyed) {
+        return;
+      }
+
       const edges = node.getEdges();
       edges.forEach((edge) => {
         // 置顶
@@ -244,6 +297,31 @@ export default class G6Graph extends PureComponent {
         // 激活自定义边的状态
         $graph.setItemState(edge, "active", true);
       });
+
+      // NOTE 添加hover自定义文本节点
+      const { id, label, x, y, index } = node.get("model");
+
+      if (label.length > 12) {
+        const hoverTxt = {
+          id: `${id}-cus`,
+          type: "rect",
+          label,
+          x: x + 20,
+          y: y + (index === "p0" ? 81 : 65),
+          size: [0, 0],
+          style: {
+            // fill: "#f00",
+            // stroke: "#0f0",
+          },
+          labelCfg: {
+            position: "right",
+            offset: 0,
+          },
+        };
+
+        $graph.addItem("node", hoverTxt);
+      }
+
       $graph.paint();
     });
 
@@ -253,6 +331,17 @@ export default class G6Graph extends PureComponent {
         edge.toBack();
         $graph.setItemState(edge, "active", false);
       });
+
+      // 清除
+      $graph
+        ?.findAll(
+          "node",
+          (nodeEl) => nodeEl && /(-cus)+/g.test(nodeEl.get("model").id)
+        )
+        ?.forEach((item) => {
+          $graph.removeItem(item.get("model").id);
+        });
+
       $graph.paint();
     });
 
@@ -264,16 +353,34 @@ export default class G6Graph extends PureComponent {
   };
 
   render() {
-    const { graphWidth, graphHeight } = this.props;
+    const { graphWidth, graphHeight, nodes } = this.props;
+
+    window.onresize = () => {
+      const current = this.graphRef?.current;
+
+      // 窗口变化后重绘画布尺寸
+      if (current?.clientWidth && current?.clientHeight) {
+        this.graph.changeSize(current?.clientWidth, current?.clientHeight);
+      }
+    };
+
     return (
       <div
         ref={this.graphRef}
         style={{
           width: `${graphWidth}%`,
-          height: `${graphHeight}%`,
+          height: `calc(${graphHeight}% - 32px)`,
           background: "#eee",
         }}
-      />
+      >
+        <Spin
+          spinning={!nodes?.length}
+          style={{
+            width: this.graphRef?.current?.clientWidth,
+            height: this.graphRef?.current?.clientHeight,
+          }}
+        />
+      </div>
     );
   }
 }
